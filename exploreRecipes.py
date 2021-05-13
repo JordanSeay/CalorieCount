2c5919b19e3661566beea7febe7923bd91e4ab3f
Beau Taylor-Ladd <beautaylorladd@gmail.com>
2021-05-13 16:26:47 -0700

    Adds ability to filter by ingredient list

--- exploreRecipes.py
+++ exploreRecipes.py
@@ -71,7 +71,7 @@ def parse_amount(line):
     # for volume measurements, I'm assuming density of water for now
     # food_portion has actual volume-to-gram conversions but that might be difficult
     measurement_to_grams = {"gram": 1, "cup": 236.59, "tablespoon": 14.787, "teaspoon": 4.929, "kilogram": 1000, "pound": 453.592, "lbs": 453.592, "milligram": 0.001, "ounce": 28.3495, "oz": 28.3495, "fl": 28.3495, "litre": 1000, "liter": 1000}
-    legal_measurements = ["gram", "grams", "cup", "tablespoon", "tablespoons", "teaspoon", "teaspoons", "kilogram", "kilograms", "pound", "pounds", "lbs", "milligram", "milligrams", "ounce", "ounces", "oz", "fl", "litre", "litres", "liter", "liters"]
+    legal_measurements = ["gram", "grams", "cup", "cups", "tablespoon", "tablespoons", "teaspoon", "teaspoons", "kilogram", "kilograms", "pound", "pounds", "lbs", "milligram", "milligrams", "ounce", "ounces", "oz", "fl", "litre", "litres", "liter", "liters"]
 
     for i, word in enumerate(line):
         if word in legal_measurements and line[i-1].isdigit():
@@ -99,6 +99,7 @@ def calorie_count(path):
 
     return calories
 
+
 def count_all_recipes(path):
     """
     input: a directory to scrape for recipe.txt files
@@ -111,7 +112,7 @@ def count_all_recipes(path):
         foldername, _, lo_Files = item
         for filename in lo_Files:
                 if filename[-3:] == "txt":
-                    results[filename] = calorie_count(foldername + "\\" + filename)
+                    results[filename] = (calorie_count(foldername + "\\" + filename), walk_ingredients(foldername + "\\" + filename))
                     print(f"recipe counted {filename}")
 
     np.savez_compressed("allCounts", **results)
@@ -122,27 +123,73 @@ def filter_by_calorie(range):
     output: list of all recipes in the desired calorie range
     """
     try:
-        calorie_counts = np.load("allCounts.npz")
+        calorie_counts = np.load("allCounts.npz", allow_pickle=True)
     except:
         print("Cannot find file allCounts.npz \n Please run python .\exploreRecipes.py -s to generate allCounts.npz")
 
     out = [] 
     for key, value in calorie_counts.items():
-        calorieCount = int(value)
+        calorieCount = int(value[0])
         if calorieCount >= int(range[0]) and calorieCount <= int(range[1]):
             out.append(key)
     return out
 
 
+def print_recipes():
+    try:
+        calorie_counts = np.load("allCounts.npz", allow_pickle=True)
+    except:
+        print("Cannot find file allCounts.npz \n Please run python .\exploreRecipes.py -s to generate allCounts.npz")
+
+    out = [] 
+    for key, value in calorie_counts.items():
+        out.append((key, value))
+    return out
+
+
+def filter_by_ingredients(ingredients, subset=False):
+    """
+    input: list of desired ingredients to filter by and whether or not the recipe must only contain those ingredients
+    output: list of all recipes that either include at least one filtered ingredient, or consists of only filtered ingredients
+    """
+
+    try:
+        calorie_counts = np.load("allCounts.npz", allow_pickle=True)
+    except:
+        print("Cannot find file allCounts.npz \n Please run python .\exploreRecipes.py -s to generate allCounts.npz")
+
+    recipes = []
+
+    if subset:
+        for key, value in calorie_counts.items():
+            include = True
+            for ingredient, _ in value[1]:
+                if ingredient not in ingredients:
+                    include = False
+                    break
+            if include:
+                recipes.append(key)
+    else:
+        for key, value in calorie_counts.items():
+            for ingredient, _ in value[1]:
+                if ingredient in ingredients:
+                    recipes.append(key)
+                    break
+    return recipes
+
 
 def main(args):
     if args.saveFile is not None:
-        count_all_recipes(".\\problem3", args.saveFile)
+        count_all_recipes("test")
     if args.recipeName is not None:
         print(calorie_count(f"{args.recipeName}"))
     if args.filterByCalorie is not None:
         print(filter_by_calorie(args.filterByCalorie))
-
+    if args.filterByIngredients is not None:
+        l,b = args.filterByIngredients
+        l = l.split(',')
+        b = b.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
+        print(filter_by_ingredients(l, b))
 
 
 if __name__ == "__main__":
@@ -167,18 +214,39 @@ if __name__ == "__main__":
         nargs=2,
         help="Integers with desired calorie range (inclusive) of recipes ex. 100 2000 for range 100cal to 2000cal"
     )
+    parser.add_argument(
+        '--filterByIngredients',
+        '-i',
+        nargs=2,
+        help="List of ingredients to filter by, and whether or not the list is exhaustive. eg <\"potato,garlic,onions\" True> will search for recipes that only contain potato,garlic,onions (or a subset) and nothing else"
+
+    )
     args = parser.parse_args()
     main(args)    
 
 # Examples:
 #   To find all recipes that contain between 100 and 200 calories inclusively 
-#   To save time this uses a precomputable calorie count file called allCounts.npz
+#   To save time this uses a precomputable calorie and ingredient count file called allCounts.npz
 #   run: python .\exploreRecipes.py -f 100 200
 #   returns: ['recipe25.txt', 'recipe37.txt', 'recipe127.txt', 'recipe159.txt', 'recipe169.txt', 'recipe201.txt']
+#
+#
+#   To find all recipes that contain cumin, among other ingredients 
+#   To save time this uses a precomputable calorie and ingredient count file called allCounts.npz
+#   run: python .\exploreRecipes.py -i "cumin" False
+#   returns: ['recipe14.txt', 'recipe15.txt', 'recipe6.txt', 'recipe7.txt']
+#
 #   
+#   To find all recipes that contain a subset of chicken, tofu, parsley, chili powder, cumin, garlic, onions and shortening
+#   To save time this uses a precomputable calorie and ingredient count file called allCounts.npz
+#   run: python .\exploreRecipes.py -i "chicken,tofu,parsley,chili powder,cumin,garlic,onions,shortening" True
+#   returns: ['recipe14.txt']
+#
+#
 #   To find the calorie count of all recipes and store in allCounts.npz file 
 #   run: python .\exploreRecipes.py -s
 # 
 #   To find the calorie count of a given recipe
 #   run: python .\exploreRecipes.py -n recipes/savory_recipes/vegetarian_recipes/recipe38.txt
 #   returns: 6960.94584
+#
--- getFoodData.py
+++ getFoodData.py
@@ -10,7 +10,6 @@ def getFoodIdDf(description, foodIdFilePath="foodData/input_food.csv"):
     colList = ["sr_description", "fdc_id"]
     df = pd.read_csv(foodIdFilePath, usecols=colList)
 
-    # print(description)
     tokenizer = RegexpTokenizer(r'\w+')
     inputDescriptionTokensCount = Counter(
         tokenizer.tokenize(description.lower()))
@@ -25,11 +24,6 @@ def getFoodIdDf(description, foodIdFilePath="foodData/input_food.csv"):
             if numMatches > maxMatches:
                 maxMatches = numMatches
                 bestMatch = row["fdc_id"]
-                # print("\n")
-                # print(description)
-                # print(bestMatch)
-                # print(descriptionTokensCount)
-                # print("\n")
 
     return bestMatch
 
@@ -37,7 +31,6 @@ def getFoodIdDf(description, foodIdFilePath="foodData/input_food.csv"):
 def getFoodIdCsv(description, foodIdFilePath="foodData/input_food.csv"):
     openFoodIdFile = open(foodIdFilePath)
 
-    # print(description)
     tokenizer = RegexpTokenizer(r'\w+')
     inputDescriptionTokensCount = Counter(
         tokenizer.tokenize(description.lower()))
@@ -45,20 +38,13 @@ def getFoodIdCsv(description, foodIdFilePath="foodData/input_food.csv"):
     bestMatch = "No Match"
 
     for row in csv.reader(openFoodIdFile):
-        # descriptionTokens = re.split("_|, | |-|!", row[2]) #Reg ex tokenizer
         descriptionTokensCount = Counter(tokenizer.tokenize(row[6].lower()))
-        # print(descriptionTokensCount)
         matches = descriptionTokensCount & inputDescriptionTokensCount
         if len(descriptionTokensCount) > 0:
             numMatches = sum(matches.values()) / len(descriptionTokensCount)
             if numMatches > maxMatches:
                 maxMatches = numMatches
                 bestMatch = row[1]
-                # print("\n")
-                # print(description)
-                # print(bestMatch)
-                # print(descriptionTokensCount)
-                # print("\n")
                 
     return bestMatch
 
